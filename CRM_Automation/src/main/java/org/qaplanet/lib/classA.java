894b25aee0f6eefdc5a3bb6743b836741b0e7b6e
qaplanet <qaplanet@gmail.com>
2018-05-13 11:18:14 +0530

    Initial POC

---
+++ CRM_Automation/src/main/java/org/qaplanet/lib/classA.java
@@ -0,0 +1,17 @@
+package org.qaplanet.lib;
+
+public class classA
+{
+	public void method1()
+	{
+		System.out.println("Method1");
+	}
+	public void method2()
+	{
+		System.out.println("Method2");
+	}
+	public void method3()
+	{
+		System.out.println("Method3");
+	}
+}
---
+++ CRM_Automation/src/main/java/org/qaplanet/lib/classB.java
@@ -0,0 +1,12 @@
+package org.qaplanet.lib;
+
+public class classB {
+	public void method1()
+	{
+		System.out.println("Method1");
+	}
+	public void method2()
+	{
+		System.out.println("Method2");
+	}
+}
